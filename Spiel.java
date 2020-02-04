05d34c535eeb42a17e6b67e8601c988129c88b5e
Codeboum <59938057+codeboum@users.noreply.github.com>
2020-02-04 09:25:22 +0100

    Gameloop hinzugefügt, Schwarzer Hintergrund

--- Spiel.java
+++ Spiel.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 
 public class Spiel extends Canvas implements Runnable{
@@ -10,12 +12,36 @@ public class Spiel extends Canvas implements Runnable{
     private Thread thread;
     private boolean laufend;
 
+    int fps = 0;
+
     private Fenster fenster;
 
     public Spiel() {
         laufend = false;
 
         fenster = new Fenster(this);
+    }
+
+
+    public void tick() {
+
+    }
+
+    public void render() {
+        BufferStrategy buffer = this.getBufferStrategy();
+        if (buffer == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics gfx = buffer.getDrawGraphics();
+
+        gfx.setColor(Color.BLACK);
+        gfx.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+
+        gfx.dispose();
+        buffer.show();
     }
 
 
@@ -35,7 +61,31 @@ public class Spiel extends Canvas implements Runnable{
     }
 
     public void run() {
-        System.out.println("Run");
+        requestFocus();
+        long letztZeit = System.nanoTime();
+        double tickAnzahl = 60.0;
+        double ns = 1000000000 / tickAnzahl;
+        double delta = 0.0;
+        long uhr = System.currentTimeMillis();
+        int frames = 0;
+
+        while (laufend) {
+            long jetzt = System.nanoTime();
+            delta += (jetzt - letztZeit) / ns;
+            letztZeit = jetzt;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (laufend) render();
+            frames++;
+            if (System.currentTimeMillis() - uhr > 1000) {
+                uhr += 1000;
+                fps = frames;
+                frames = 0;
+            }
+        }
+        anhalten();
     }
 
 
